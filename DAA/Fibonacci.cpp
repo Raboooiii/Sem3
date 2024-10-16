3d6e89ec46bee7cae5a7562e0a29ca1c8901f9ee
Rabeah Basheer <146197313+Raboooiii@users.noreply.github.com>
2024-10-16 15:33:12 +0530

    Create Fibonacci.cpp

---
+++ DAA/Fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void fibonacci(int n) {
+    int f1 = 0, f2 = 1, F;
+    cout << "Fibonacci Series: " << f1 << " " << f2 << " ";
+    for (int i = 1; i <= n-2; ++i) {
+        F = f1 + f2;
+        cout << F << " ";
+        f1 = f2;
+        f2 = F;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    fibonacci(n);
+    return 0;
+}
