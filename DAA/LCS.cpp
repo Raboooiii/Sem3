cdaf182a17e5cc4e097beb39e9e1d846f59fef95
Rabeah Basheer <146197313+Raboooiii@users.noreply.github.com>
2024-10-30 14:18:14 +0530

    .

--- DAA/LCS.cpp
+++ DAA/LCS.cpp
@@ -2,36 +2,33 @@
 #include <cstring>
 using namespace std;
 
-const int MAX = 100; // Maximum length for the strings
+const int MAX = 100; 
 
-// Function to find the length of the longest common subsequence
 int longestCommonSubsequence(char X[], char Y[], int m, int n) {
-    int f[MAX][MAX] = {0}; // DP table initialized to 0
+    int f[MAX][MAX] = {0}; 
 
-    // Build the DP table
     for (int i = 1; i <= m; i++) {
         for (int j = 1; j <= n; j++) {
             if (X[i - 1] == Y[j - 1]) {
-                f[i][j] = f[i - 1][j - 1] + 1; // Characters match
+                f[i][j] = f[i - 1][j - 1] + 1; 
             } else {
-                f[i][j] = max(f[i - 1][j], f[i][j - 1]); // Take max
+                f[i][j] = max(f[i - 1][j], f[i][j - 1]); 
             }
         }
     }
 
-    return f[m][n]; // Return length of LCS
+    return f[m][n];
 }
 
 int main() {
-    char X[MAX], Y[MAX]; // Input strings X and Y
-
+    char X[MAX], Y[MAX]; 
+    
     cout << "Enter first string (X): ";
-    cin >> X; // Input the first string
+    cin >> X; 
     cout << "Enter second string (Y): ";
-    cin >> Y; // Input the second string
-
-    int m = strlen(X); // Length of string X
-    int n = strlen(Y); // Length of string Y
+    cin >> Y; 
+    int m = strlen(X); 
+    int n = strlen(Y); 
 
     cout << "Length of LCS: " << longestCommonSubsequence(X, Y, m, n) << endl;
     return 0;
