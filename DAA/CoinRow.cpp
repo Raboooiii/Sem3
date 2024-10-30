06ebb0a570b1a8c489809f39dd6ce49bcde795ff
Rabeah Basheer <146197313+Raboooiii@users.noreply.github.com>
2024-10-30 14:10:13 +0530

    Update CoinRow.cpp

--- DAA/CoinRow.cpp
+++ DAA/CoinRow.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int maxMoney(const vector<int>& coins){
-	int n = coins.size();
-	if (n == 0){
-		return 0;
-	}
-	if (n == 1){
-		return coins[0];
-	}
-	if (n == 2){
-		return max(coins[0],coins[1]);
-	}
-	
-	vector<int> f(n);
-	f[0] = coins[0];
-	f[1] = max(coins[0], coins[1]);
-	
-	for (int i = 2; i < n; i++){
-		f[i] = max(f[i - 1], f[i - 2] + coins[i]);
-	}
-	return f[n - 1];
+void coin_row (int C[],int n){
+    int F[n],B[n];
+    F[0] = 0;
+    F[1] = C[1];
+    B[0] = 0;
+    B[1] = 1;
+    for(int i =2;i<n;i++){
+        if(C[i]+F[i-2]>F[i-1]){
+            B[i] = i-2;
+        }
+        else{
+            B[i] = i-1;
+        }
+        F[i] = max(C[i]+F[i-2],F[i-1]);
+    }
+    cout<<"Max Value :"<<F[n-1]<<endl;
+    cout<<"Chosen Coins: ";
+    for(int i=n;i>=0;){
+        if(B[i] == i-2){
+            cout<<C[i]<<" ";
+            i = i-2;
+        }
+        else if(i==1){
+            cout<<C[i];
+            i = i-1;
+        }
+        else{
+            i--;
+        }
+    }
+    cout<<endl;
 }
 
 int main(){
-	int n;
-	cout<<"How many coins are there :";
-	cin>>n;
-	vector <int> coins(n);
-	cout<<"Enter the values of the coins :";
-	for(int i = 0; i<n;i++){
-		cin>>coins[i];
-	}
-	cout << "Maximum money that can be collected: " << maxMoney(coins) << endl;
-	
-	return 0;
+    int n;
+    cout<<"Enter number Of Coins:";
+    cin>>n;
+    int A[n];
+    cout<<"Enter Coins :";
+    A[0] = 0;
+    for(int i=1;i<=n;i++){
+        cin>>A[i];
+    }
+    coin_row(A,n+1);
+    return 0;
 }
