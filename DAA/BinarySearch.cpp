25741a23228abeb8f7471210f7bb8ee6b6dc3b04
Rabeah Basheer <146197313+Raboooiii@users.noreply.github.com>
2024-11-03 18:37:39 +0530

    Update BinarySearch.cpp

--- DAA/BinarySearch.cpp
+++ DAA/BinarySearch.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int BinarySearch(int arr[], int LB, int UB, int x){
-    if(LB > UB){
-        return -1;
+int BinarySearch(int arr[], int LB, int UB, int x) {
+    if(LB <= UB) {
+        int mid = LB + (UB - LB) / 2;
+        if(arr[mid] == x) {
+            return mid;
         }
-    int mid = (LB + UB)/2;
-    if(arr[mid] == x){
-        return mid;
-    }
-    else if(arr[mid] > x){
-        return BinarySearch(arr, LB, mid-1, x);
-    }
-    else{
+        if(arr[mid] > x) {
+            return BinarySearch(arr, LB, mid-1, x);
+        }
         return BinarySearch(arr, mid+1, UB, x);
     }
-    
+    return -1;
 }
-int main(){
-    int arr[100],n,x;
-    cout<<"Enter the size of the array: ";
-    cin>>n;
-    cout<<"Enter the elements of the array: ";
-    for(int i = 0; i < n; i++){
-        cin>>arr[i];
-        }
-    cout<<"Enter the element to search: ";
-    cin>>x;
-    int LB = 0, UB = n-1;
-    int index = BinarySearch(arr, LB, UB, x);
-    if(index == -1){
-        cout<<"Element not found";
+
+int main() {
+    int n, x;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    else{
-        cout<<"Element found at index "<<index<<endl;
+    
+    cout << "Enter the element to search: ";
+    cin >> x;
+    
+    int index = BinarySearch(arr, 0, n-1, x);
+    
+    if(index == -1) {
+        cout << "Element not found";
+    } else {
+        cout << "Element found at index " << index;
     }
+    
     return 0;
 }
